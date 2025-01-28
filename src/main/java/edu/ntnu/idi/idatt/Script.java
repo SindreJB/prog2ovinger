9dbe5e49e0905c8ab036ccd87ba3fc44d8f4bf3b
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-01-28 17:45:43 +0100

    Successful test of replaceText

--- src/main/java/edu/ntnu/idi/idatt/Script.java
+++ src/main/java/edu/ntnu/idi/idatt/Script.java
@@ -1,5 +1,8 @@
 package edu.ntnu.idi.idatt;
 
+import edu.ntnu.idi.idatt.CapitalizeText.CapitalizeTextCommand;
+import edu.ntnu.idi.idatt.ReplaceText.ReplaceFirstTextCommand;
+import edu.ntnu.idi.idatt.ReplaceText.ReplaceTextCommand;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,14 +14,22 @@ public class Script {
       this.textCommands = new ArrayList<>();
   }
 
-  public void addCommand(TextCommand textCommand) {
-      textCommands.add(textCommand);
-  }
-
   public String execute(String text) {
     final String[] result = {text};
     textCommands.stream()
         .forEach(textCommand -> result[0] = textCommand.execute(result[0]));
     return result[0];
+  }
+
+  public void customScript() {
+    String text = "Hello Hello, world!";
+    System.out.println(text);
+    TextCommand replaceTextCommand = new ReplaceTextCommand("Hello", "Hi");
+    System.out.println(replaceTextCommand.execute(text));
+
+    String text2 = "Hello Hello, world!";
+    System.out.println(text2);
+    TextCommand replaceFirstTextCommand = new ReplaceFirstTextCommand("Hello", "Hi");
+    System.out.println(replaceFirstTextCommand.execute(text));
   }
 }
--- src/main/java/edu/ntnu/idi/idatt/TextAPI.java
+++ src/main/java/edu/ntnu/idi/idatt/TextAPI.java
@@ -3,10 +3,8 @@ package edu.ntnu.idi.idatt;
 public class TextAPI {
 
   public static void main(String []args) {
-    String text = "Hello, World!";
-    TextCommand textCommand = new ToUpperCaseCommand();
-
-
+    Script script = new Script();
+    script.customScript();
   }
 
 }
