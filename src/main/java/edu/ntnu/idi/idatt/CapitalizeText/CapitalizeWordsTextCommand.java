14fc60b07464e9cedccd56e1d779818c9d1f31a8
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-02-18 17:24:47 +0100

    Logic for CapitalizeWordsTextCommand

--- src/main/java/edu/ntnu/idi/idatt/CapitalizeText/CapitalizeWordsTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/CapitalizeText/CapitalizeWordsTextCommand.java
@@ -1,16 +1,16 @@
 package edu.ntnu.idi.idatt.CapitalizeText;
 
+import java.util.stream.Stream;
+
 public class CapitalizeWordsTextCommand extends CapitalizeTextCommand {
 
   @Override
   public String execute(String text) {
     String[] words = text.split(" ");
-    StringBuilder sb = new StringBuilder();
-    for (String word : words) {
-      sb.append(word.substring(0, 1).toUpperCase());
-      sb.append(word.substring(1));
-      sb.append(" ");
-      }
-    return sb.toString().trim();
+    StringBuilder capitalizedText = new StringBuilder();
+    Stream.of(words).forEach(word -> {
+      capitalizedText.append(super.execute(word)).append(" ");
+    });
+    return capitalizedText.toString().trim();
   }
 }
