291eabfa915c74a74eddd371aaab6cf1e18143b7
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-02-27 13:28:05 +0100

    small changes

--- src/main/java/edu/ntnu/idi/idatt/Script.java
+++ src/main/java/edu/ntnu/idi/idatt/Script.java
@@ -31,47 +31,55 @@ public class Script {
     System.out.println(text);
     TextCommand replaceTextCommand = new ReplaceTextCommand("Hello", "Hi");
     System.out.println(replaceTextCommand.execute(text));
+    System.out.println(" ");
 
     System.out.println("Replacing first Hello with Hi");
     String text2 = "Hello Hello, world!";
     System.out.println(text2);
     TextCommand replaceFirstTextCommand = new ReplaceFirstTextCommand("Hello", "Hi");
     System.out.println(replaceFirstTextCommand.execute(text2));
+    System.out.println(" ");
 
     System.out.println("wrapping text with <p>");
     String text3 = "Hello, world!";
     System.out.println(text3);
     TextCommand wrapTextCommand = new WrapTextCommand("<p>", "<p>");
     System.out.println(wrapTextCommand.execute(text3));
+    System.out.println(" ");
 
     System.out.println("wrapping lines with <p>");
     String text4 = "Hello, world!\nHow are you?";
     System.out.println(text4);
     TextCommand wrapLinesTextCommand = new WrapLinesTextCommand("<p>", "<p>");
     System.out.println(wrapLinesTextCommand.execute(text4));
+    System.out.println(" ");
 
     System.out.println("wrapping selection with <p>");
     String text5 = "Hello, world!";
     System.out.println(text5);
     TextCommand wrapSeletionTextCommand = new WrapSelectionTextCommand("<p>", "<p>", "world");
     System.out.println(wrapSeletionTextCommand.execute(text5));
+    System.out.println(" ");
 
     System.out.println("capitalizing first letter of the text");
     String text6 = "hello, world!";
     System.out.println(text6);
     TextCommand capitalizeTextCommand = new CapitalizeTextCommand();
     System.out.println(capitalizeTextCommand.execute(text6));
+    System.out.println(" ");
 
     System.out.println("capitalizing first letter of every word in the text");
     String text7 = "hello, world!";
     System.out.println(text7);
     TextCommand capitalizeWordsTextCommand = new CapitalizeTextCommand();
     System.out.println(capitalizeWordsTextCommand.execute(text7));
+    System.out.println(" ");
 
     System.out.println("capitalizing selected words in the text");
     String text8 = "hello, world! Goodbye, world!";
     System.out.println(text8);
     TextCommand capitalizeSelectionTextCommand = new CapitalizeSelectionTextCommand("world");
     System.out.println(capitalizeSelectionTextCommand.execute(text8));
+    System.out.println(" ");
   }
 }
