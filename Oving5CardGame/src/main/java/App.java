477d9967e5a3ad1a8a5cbb2ed5fa4534ff56a2d0
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-03-20 11:47:06 +0100

    Deleted code for terminal game

--- Oving5CardGame/src/main/java/App.java
+++ Oving5CardGame/src/main/java/App.java
@@ -4,12 +4,6 @@ import view.GamePage;
 public class App {
 
   public static void main(String[] args) {
-//    DeckOfCards deck = new DeckOfCards();
-//    deck.dealHand(5);
-//    System.out.println(" ");
-//    System.out.println("Remaining cards in the deck:");
-//    deck.printDeck();
-
     GamePage.main(args);
   }
 }
